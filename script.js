d23cbf5989bd0192da862a9e6825b17bf172b5ed
Saurabh Pandit <91050605+Saurabhsp1@users.noreply.github.com>
2023-11-28 15:41:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+    const checkbox = document.getElementById("myCheckbox");
+
+    checkbox.addEventListener("change", function() {
+        if (checkbox.checked) {
+            alert("Checkbox is checked!");
+        } else {
+            alert("Checkbox is unchecked!");
+        }
+    });
+
+
+    /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+
+
+  
